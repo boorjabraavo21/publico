9d5d1227a6f2b9abd8793b04fbbb35ddbc63b468
Boorja21 <115183112+boorjabraavo21@users.noreply.github.com>
2023-05-02 16:54:15 +0200

    a

--- jscajas/jscajas.js
+++ jscajas/jscajas.js
@@ -1,31 +1,31 @@
 'use strict'; // nos ayuda a no pasar ningún error
 // pedimos en que posicion escribir HOLA
-var numero = prompt('¿En qué posición quiere colocar la palabra? (1 a 5)', '');
-numero = parseInt(numero);
+var color = "white";
 
-var color = prompt('¿De qué color quiere la pirámide? (r,b,g,y,o)','');
-// cuadro tendrá que estar entre 1 y 5
-var selector = "d"+numero;
-var esteCuadro = document.getElementById(selector);
-esteCuadro.innerHTML="__o__<br>_ooo_<br>ooooo";
+var colorFr = ['black','white','violet','brown','grey'];
+var colorBr = ['red','blue','green','yellow','orange'];
 
-switch (color) {
-    case "r":
-        esteCuadro.style.color="red";
-        break;
-    case "b":
-        esteCuadro.style.color="blue";
-        break;
-    case "g":
-        esteCuadro.style.color="green";
-        break;
-    case "y":
-        esteCuadro.style.color="yellow";
-        break;
-    case "o":
-        esteCuadro.style.color="orange";
-        break;
-    default:
-        esteCuadro.style.color="white";
-        break;
+function pinturaInicial() {
+    for (var i = 0; i < 5; i++) {
+        var identificador = "d"+(i+1);
+        var elemento = document.getElementById(identificador);
+        elemento.style.backgroundColor = colorBr[i];
+        elemento.style.color = colorFr[i];
+    }
 }
+
+function obtieneColor(identificador) {
+    var id="d"+identificador;
+    var cuadro = document.getElementById(id);
+    color = getComputedStyle(cuadro).backgroundColor;
+    console.log(cuadro);
+}
+
+function saludar(identificador) {
+    var id = "d"+identificador;
+    var cuadro = document.getElementById(id);
+    cuadro.style.color = color;
+    cuadro.innerHTML="HOLA";
+}
+
+pinturaInicial();
